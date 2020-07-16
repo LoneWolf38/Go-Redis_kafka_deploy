33d6b3d96cb5b773e31f4b162320b25149e0248a
LoneWolf38 <sudodeba97.iiitbbsr@gmail.com>
2020-07-16 19:50:08 +0530

    Readme updated

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -44,3 +44,11 @@ variable "admin_cidr_ingress" {
 variable "image_id"{
   description = "AMI ID"
 }
+
+variable "aws_access_key"{
+  description = "Access Key"
+}
+
+variable "aws_secret_key"{
+  description = "Secret Key"
+}
